d8dbc7b3461e7334e7052a91a76a4b8062ea2c79
tiger0427 <capacitor0427@gmail.com>
2022-03-02 17:20:27 +0900

    add: deleteUserPublic

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -144,3 +144,31 @@ export const createUserPublic = functions
       };
       await db.collection("userPublic").doc(user.uid).set(userPublic);
     });
+
+export const updateUserPublic = functions
+    .region("asia-northeast2")
+    .firestore.document("user/{uid}")
+    .onUpdate(async (snapshot, context) => {
+      const app = admin.initializeApp(undefined, context.eventId);
+      const db = app.firestore();
+      const user = snapshot.after.data() as User;
+      const userPublic: UserPublic = {
+        faculty: user.faculty,
+        name: user.name,
+        grade: user.grade,
+        gender: user.gender,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      };
+      await db.collection("userPublic").doc(user.uid).update(userPublic);
+    });
+
+export const deleteUserPublic = functions
+    .region("asia-northeast2")
+    .firestore.document("user/{uid}")
+    .onDelete(async (snapshot, context) => {
+      const app = admin.initializeApp(undefined, context.eventId);
+      const db = app.firestore();
+      const user = snapshot.data() as User;
+      await db.collection("userPublic").doc(user.uid).delete();
+    });
