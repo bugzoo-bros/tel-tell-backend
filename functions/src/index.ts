43535b01f5a9a6adec2dfe7f4ac9dde07ade6dab
tiger0427 <capacitor0427@gmail.com>
2022-04-15 19:32:53 +0900

    Add updateTotalOnlineUsers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,8 +2,10 @@ import * as _agora from "./agora";
 import * as _user from "./user";
 import * as _match from "./match";
 import * as _block from "./block";
+import * as _presence from "./presence";
 
 export const agora = _agora;
 export const user = _user;
 export const match = _match;
 export const block = _block;
+export const presence = _presence;
